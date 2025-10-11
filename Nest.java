6575ae6e24860272dd4472b28cb9e767b8ab9faa
Sathwik <tsathwikreddy7@gmail.com>
2025-10-11 22:18:50 +0530

    Create Nest.java

---
+++ Nest.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Nest
+{
+	public static void main(String[] args)
+	{
+		Scanner in=new Scanner(System.in);
+		int n,r,h=0;
+		n=in.nextInt();
+		r=n;
+		while(n!=0)
+		{
+             if(n%10==0){
+                 h++;
+             }
+			n=n/10;
+            
+		}
+		if(h>=1)
+         System.out.println("Nest");
+         else 
+         System.out.println("Not nest");
+	}
+}
