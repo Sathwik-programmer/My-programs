5d7d6cf9e0bd0e420a232a3e34b04c463c1970ef
Sathwik <tsathwikreddy7@gmail.com>
2025-12-07 06:11:42 +0000

    Refactor formatting of main method and loops in Triangle_pattern3.java

--- Nested-for/Triangle_pattern3.java
+++ Nested-for/Triangle_pattern3.java
@@ -1,9 +1,12 @@
 public class Triangle_pattern3 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int rows = 5;
 
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 1; i <= rows; i++) 
+        {
+            for (int j = 1; j <= i; j++)
+         {
                 System.out.print(j + " ");
             }
             System.out.println();
